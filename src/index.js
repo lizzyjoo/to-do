c947ac000009f9816316e119ca46a764155461f7
lizzyjoo <jejviolinist@gmail.com>
2024-07-24 20:44:51 -0400

    load default projects only, no default tasks

--- src/index.js
+++ src/index.js
@@ -1,38 +1,9 @@
 import "./style.css"; 
-import { subYears, addDays, addMonths, addYears, format } from 'date-fns'
-
 import UI from "./ui";
 import Task from "./task";
 import Project from "./project";
 import Storage from "./storage";
 
-const createDefaultTasks = () => {
-    const today = new Date();
-    const tasks = [
-        {
-        content: 'Water the plants',
-        priority: 'high',
-        dueDate: format(subYears(today, new Date().getFullYear()), 'yyyy-MM-dd'), // Current year years ago
-        parentProjectId: '1'
-        },
-        {
-        content: 'Go to trader joes for groceries',
-        priority: 'medium',
-        dueDate: format(addDays(today, -1), 'yyyy-MM-dd'), // Yesterday
-        parentProjectId: '1'
-        },
-        {
-        name: 'Do laundry',
-        priority: 'low',
-        dueDate: format(today, 'yyyy-MM-dd'), // Today
-        parentProjectId: '1'
-        },
-
-    ]
-
-    return tasks;
-
-}
 
 const createDefaultProjects = () => {
     const projects = [
@@ -80,9 +51,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // Default Projects
     const defaultProjects = createDefaultProjects();
-    defaultProjects.forEach((project) => Storage.addProject(project));
-    UI.initialize()
-    // Default tasks
-    const defaultTasks = createDefaultTasks();
+    defaultProjects.forEach((project) => {
+        
+        Storage.addProject(project)
+    });
 
+    UI.initialize()
+    
+  
   });
